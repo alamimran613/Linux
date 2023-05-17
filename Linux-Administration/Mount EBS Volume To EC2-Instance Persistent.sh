d5cafb2e139f30627634d8c518a5895ac07151c1
Imran Alam <112178589+alamimran613@users.noreply.github.com>
2023-05-17 18:01:10 +0530

    update in files

--- Linux-Administration/Mount EBS Volume To EC2-Instance Persistent.sh
+++ Linux-Administration/Mount EBS Volume To EC2-Instance Persistent.sh
@@ -1,4 +1,6 @@
 # Mount EBS Volume on EC2 Instance Persistent.
+# Follow link for referrence - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-using-volumes.html
+# This is ext4 filesystem type volume mount
 # Create volume and attach with ec2 isntance.
 
 # Step 1: Now, ssh to your ec2 instance and list the available disks using the following command.
@@ -8,6 +10,8 @@
 # Step 2: Check if the volume has any data using the following command.
 
 sudo file -s /dev/xvdf
+
+# If the above command output shows “/dev/xvdf: data“, it means your volume is empty.
 
 # Step 3: Format the volume to the ext4 filesystem using the following command.
           # We can use another file system as per our need i.e. xfs, ext4, btrfs, zfs
@@ -19,41 +23,54 @@ sudo mkfs -t ext4 /dev/xvdf
 
 sudo mkdir /data
 
-# Step 5: Mount the volume to “data” directory using the following command.
+# Step 5: Mount the volume to the directory you created.
+
+# Step 6: Mount the volume to “data” directory using the following command.
 
 sudo mount /dev/xvdf /data/
 
-# Step 6: cd into data directory and check the disk space to validate the volume mount.
+# Step 7: cd into data directory and check the disk space to validate the volume mount.
 
-cd /data
-df -h
+# cd /data
+# du -h
 
-# Step 7: unmount the volume then you can use below command and verify using lsblk command
+# Step 8: unmount the volume then you can use below command and verify using lsblk command
 
 sudo umount /dev/xvdf
 lsblk
 
 #-----------Automount EBS volume on Reboot. Make persistent mounted volume------------------
 
-# Step 8: Back up the /etc/fstab file if encounter any kind of error we will restore it.
+# Step 9: Back up the /etc/fstab file if encounter any kind of error we will restore it.
 
-sudo cp /etc/fstab /etc/fstab.bak
+sudo cp -v /etc/fstab /etc/fstab.bak
 
-# Step 9: Open /etc/fstab file and make an entry in the following format.
+# Step 10: Open /etc/fstab file and make an entry in the following format.
         # device_name mount_point file_system_type fs_mntops fs_freq fs_passno
 
  # Example: /dev/xvdf       /data   ext4    defaults,nofail        0       0
- # Then save and close the file
 
-# Step 10: Execute the following command to check if the fstab file has any errors.
+ # Then save and close the file or you can use below command to append this line direct into /etc/fstab file
+
+echo "/dev/xvdf  /data  ext4  defaults,nofail  0   0" | sudo tee -a /etc/fstab
+
+# Step 11: Execute the following command to check if the fstab file has any errors.
 
 sudo mount -a
 
 # If the above command shows no error, it means our fstab entry is valid.
 
-# Step 11: Check and verify mounted volume
+# Step 12: Check and verify mounted volume
 
 lsblk
+
+# Step 13: For give permission for user to this directory
+
+sudo chown -R ec2-user:ec2-user /data
+
+du -h /data
+
+ls -l /data
 
 # Now, on every reboot, the extra EBS volumes will get mounted automatically.
 
@@ -61,7 +78,7 @@ lsblk
 
 # Important Informations:
 
-# fstab
+# fstab:
 
 # `fstab` stands for "file systems table" and is a configuration file used in Linux and other Unix-based operating systems to define how file systems are mounted at boot time. The `fstab` file contains information about the file systems that are mounted at boot time, including the file system type, mount point, options, and other parameters.
 
@@ -71,3 +88,10 @@ lsblk
 
 # The `fstab` file is an important configuration file in Linux, and it is important to ensure that it is correctly configured to avoid any issues with mounting file systems at boot time.
 
+#----------------------------------------------------------------------------
+
+# mkfs:
+
+# In the context of the `mkfs` command, the `-t` option is used to specify the type of filesystem to be created on the given device. In this specific command, `-t ext4` specifies that the `ext4` filesystem type should be used.
+
+# The `mkfs` command is used to create a filesystem on a device (such as a hard disk or USB drive) so that it can be used to store files and directories. The `/dev/xvdf` argument specifies the device that the filesystem should be created on. The `sudo` command is used to run the `mkfs` command with administrative privileges.
